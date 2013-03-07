00e6886293497142ab66d8531b2373d03c9a8d0e
James Eady <jmeady@gmail.com>
2013-03-06 22:08:39 -0800

    Change gruntfile to also include lib/controls subdirectory. I soon be doing some refactoring and will add things to the lib/controls directory.

--- grunt.js
+++ grunt.js
@@ -4,18 +4,18 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     lint: {
-      js: ['grunt.js', 'lib/*.js']
+      js: ['grunt.js', 'lib/controls/*.js', 'lib/*.js']
     },
     csslint: {
-      css: 'lib/*.css'
+      css: ['lib/*.css', 'lib/controls/*.css']
     },
     concat: {
       js: {
-        src: ['lib/*.js', 'vendor/**/*.js'],
+        src: ['lib/*.js', 'lib/controls/*.js', 'vendor/**/*.js'],
         dest: 'sticigui.js'
       },
       css: {
-        src: ['lib/*.css', 'vendor/**/*.css'],
+        src: ['lib/*.css', 'lib/controls/*.css', 'vendor/**/*.css'],
         dest: 'sticigui.css'
       }
     },
